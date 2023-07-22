f49d018ca08a0d514dcddc7e444f0656a18e9a75
Dominik Kormann <dominikormann@outlook.com>
2023-07-22 17:22:06 +0200

    title

--- src/routes/store.ts
+++ src/routes/store.ts
@@ -1,15 +1,15 @@
-import { browser } from "$app/environment";
-import { writable, type Writable ,get} from "svelte/store";
+import { browser } from '$app/environment';
+import { writable, type Writable, get } from 'svelte/store';
 
 export function createWritable<T>(key: string, defaultValue: T) {
     var value: T;
     if (browser) {
-        if (typeof defaultValue === "string") {
+        if (typeof defaultValue === 'string') {
             value = localStorage[key] ?? defaultValue;
         } else {
             let defaultJSON = JSON.stringify(defaultValue);
             let storedJSON = localStorage[key];
-            if (storedJSON === "undefined") {
+            if (storedJSON === 'undefined') {
                 localStorage[key] = defaultJSON;
                 storedJSON = defaultJSON;
             }
@@ -27,7 +27,7 @@ export function createWritable<T>(key: string, defaultValue: T) {
 
     if (browser) {
         wr.subscribe((value) => {
-            if (typeof value === "string") {
+            if (typeof value === 'string') {
                 localStorage[key] = value;
             } else {
                 localStorage[key] = JSON.stringify(value);
@@ -37,22 +37,22 @@ export function createWritable<T>(key: string, defaultValue: T) {
 
     return wr;
 }
-export const useserver = createWritable("useserver", false);
-export const openaikey = createWritable("openaikey", "");
+export const useserver = createWritable('useserver', false);
+export const openaikey = createWritable('openaikey', '');
 
 if (browser) {
-    if (get(openaikey) == "") {
-        let usrinput = (prompt("please enter your openai key"));
+    if (get(openaikey) == '') {
+        let usrinput = prompt('please enter your openai key');
         if (usrinput != null) {
             openaikey.set(usrinput);
         }
     }
 }
 
-export const messagecount = createWritable("messagecount", 0);
+export const messagecount = createWritable('messagecount', 0);
 
 export type chatMessage = {
-    role: "user" | "assistant" | "system";
+    role: 'user' | 'assistant' | 'system';
     content: string;
 };
 
@@ -67,10 +67,12 @@ export type MessageNodeId = number;
 let database: { [keys: MessageNodeId]: Writable<MessageNode> } = {};
 
 if (browser) {
-    if (!localStorage.getItem("message_0")) {
-        createNode({ role: "assistant", content: "how can i help you" }, null);
-        createNode({ role: "user", content: "hello" }, 0);
-        createNode({ role: "user", content: "bye" }, 0);
+    if (!localStorage.getItem('message_0')) {
+        createNode({ role: 'assistant', content: 'how can I help you?' }, null);
+        createNode({ role: 'user', content: 'explain quantum computing for a five year old' }, 0);
+        createNode({ role: 'user', content: 'say "I love you" in binary' }, 0);
+        createNode({ role: 'user', content: 'show me a spline in python' }, 0);
+        createNode({ role: 'user', content: '...' }, 0);
     }
 }
 
@@ -79,17 +81,16 @@ export function getNode(id: MessageNodeId) {
         if (browser) {
             database[id] = createWritable<MessageNode>(`message_${id}`, {
                 message: {
-                    role: "assistant",
-                    content: "how can I help you?",
+                    role: 'assistant',
+                    content: 'how can I help you?'
                 },
                 children: [],
-                parent: null,
+                parent: null
             });
         }
     }
     return database[id];
 }
-
 
 export function createNode(message: chatMessage, parent: MessageNodeId | null) {
     let id = 0;
@@ -97,13 +98,13 @@ export function createNode(message: chatMessage, parent: MessageNodeId | null) {
         id = value;
         return value + 1;
     });
-    console.log("creating ", id);
+    console.log('creating ', id);
 
     let node = getNode(id);
     node?.set({
         message: message,
         children: [],
-        parent: parent,
+        parent: parent
     });
 
     if (parent != null) {
@@ -115,24 +116,21 @@ export function createNode(message: chatMessage, parent: MessageNodeId | null) {
 }
 
 export function deleteNode(id: MessageNodeId) {
-
     if (id == 0) {
         return;
     }
 
     let node = get(getNode(id));
 
-    if (node.parent == 0 && get(getNode(0)).children.length==1){
-        createNode({role:"user", content:""}, 0)
+    if (node.parent == 0 && get(getNode(0)).children.length == 1) {
+        createNode({ role: 'user', content: '' }, 0);
     }
 
-    
-    console.log("deleting ", id);
-    if (node.parent!=null){
+    console.log('deleting ', id);
+    if (node.parent != null) {
         let parentnode = getNode(node.parent);
-        parentnode.update((p) => ({ ...p, children: p.children.filter((x)=>x!=id) }));
-        console.log("parent ", get(parentnode));
-        
+        parentnode.update((p) => ({ ...p, children: p.children.filter((x) => x != id) }));
+        console.log('parent ', get(parentnode));
     }
 
     node.children.forEach((child) => {
@@ -141,5 +139,4 @@ export function deleteNode(id: MessageNodeId) {
 
     localStorage.removeItem(`message_${id}`);
     delete database[id];
-    
 }
